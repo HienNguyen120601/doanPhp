b5b6ee51a4b61b4600f955653f7c4bba18d5b7ea
Hiennguyen120601 <nguyen.nguyenhienth05@gmail.com>
2022-12-02 16:56:13 +0700

    add db

--- sql/connect.php
+++ sql/connect.php
@@ -1,5 +1,6 @@
 <?php
-try {
-    $pdo = new PDO("mysql:host=localhost;dbname=khago", "root", "");
-} catch (PDOException $e) {
-}
+  $dsn = "mysql:host={$_ENV["HOST"]};dbname={$_ENV["DATABASE"]}";
+  $options = array(
+    PDO::MYSQL_ATTR_SSL_CA => "/etc/ssl/certs/ca-certificates.crt",
+  );
+  $pdo = new PDO($dsn, $_ENV["USERNAME"], $_ENV["PASSWORD"], $options);
